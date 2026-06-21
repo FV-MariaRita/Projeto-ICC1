180e7ca3d6f3c7fe7380e9fce4c969c62be8b5fa
FV-MariaRita <mritafvargas@gmail.com>
2026-06-21 12:49:55 -0300

    Venda e consultas: criação dos arquivos para construir as funções

---
+++ consultas.c
@@ -0,0 +1,23 @@
+/*
+Projeto ICC 1 - Mercadinho 
+Alunos: Chrystian Eloy, Julia Nogueira, Maria Rita Vargas
+
+Esse arquivo contém as funções de consultas ao sitema, as quais além de buscar informações do mercadinho, as exibe no terminal: 
+
+    - void consultaEstoque : responsável por consultar o estoque do sistema
+    - void consultaSaldo: responsável por consultar o saldo do caixa
+
+As informações consultadas serão exibidas no formato 
+
+Consulta estoque: 
+
+<código>_<nome>_<quantidade no estoque>
+...
+<código>_<nome>_<quantidade no estoque>
+--------------------------------------------------
+
+Consulta saldo:
+
+Saldo:_<saldo do caixa>
+--------------------------------------------------
+*/
---
+++ venda.c
@@ -0,0 +1,18 @@
+/*
+Projeto ICC 1 - Mercadinho 
+Alunos: Chrystian Eloy, Julia Nogueira, Maria Rita Vargas
+
+Esse arquivo contém as funções relativas à venda de produtos do mercadinho, a saber: 
+
+    - void realizarVenda: responsável por realizar a venda
+
+A exibição da venda é feita nos moldes:
+
+<nome>_<preço>
+...
+<nome>_<preço>
+Total:_<preço total>
+--------------------------------------------------
+*/
+
+
