9e22c4bf6f1d530d8d5b148e35d77fe3115a9a7b
Chrystian Eloy <chrystianeloy@usp.br>
2026-06-21 21:40:05 -0300

    realização da T5 - realizar uma venda

--- venda.c
+++ venda.c
@@ -1,18 +1,66 @@
 /*
-Projeto ICC 1 - Mercadinho 
+Projeto ICC 1 - Mercadinho
 Alunos: Chrystian Eloy, Julia Nogueira, Maria Rita Vargas
 
-Esse arquivo contém as funções relativas à venda de produtos do mercadinho, a saber: 
-
-    - void realizarVenda: responsável por realizar a venda
-
-A exibição da venda é feita nos moldes:
-
-<nome>_<preço>
-...
-<nome>_<preço>
-Total:_<preço total>
---------------------------------------------------
+Esse arquivo contém as funções relacionadas ao processo de
+vendas do mercadinho, incluindo a busca de produtos no estoque,
+a atualização das quantidades vendidas e do saldo do caixa.
 */
 
+#include <stdio.h>
+#include "venda.h"
+
+/*
+    Procura um produto no estoque a partir do seu código.
+    Retorna:
+    - Ponteiro para o produto encontrado.
+    - NULL caso não encontre.
+*/
+static Produto *buscarProduto(Produto *estoque, int prodsEstoque, int codigo) {
+    for (int i = 0; i < prodsEstoque; i++) {
+        if (estoque[i].id == codigo) {
+            return &estoque[i];
+        }
+    }
+
+    return NULL;
+}
+
+/*
+    Realiza uma venda.
+    Lê os códigos dos produtos até encontrar -1,
+    imprime os itens vendidos, atualiza o estoque
+    e soma o valor total ao saldo do caixa.
+*/
+void realizarVenda(Produto *estoque, int prodsEstoque, double *saldoCaixa) {
+
+    int codigo;
+    double total = 0.0;
+
+    scanf("%d", &codigo);
+
+    while (codigo != -1) {
+
+        Produto *prod =
+            buscarProduto(estoque, prodsEstoque, codigo);
+
+        printf("%s %.2lf\n", (*prod).nome, (*prod).preco);
+
+        (*prod).quantidade--;
+
+        total += (*prod).preco;
+
+        scanf("%d", &codigo);
+    }
+
+    *saldoCaixa += total;
+
+    printf("Total: %.2lf\n", total);
+
+    for (int i = 0; i < 50; i++) {
+        printf("-");
+    }
+
+    printf("\n");
+}
 
